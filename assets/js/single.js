930b7c0925b793bbe269a1b3d320e827112e5a05
Daniel Lewis <daniel.lewis@protonmail.com>
2021-02-24 17:05:22 -0700

    Add an Extra Message

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,17 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
+function displayWarning(repo){
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "see more issues on github.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
 
 function displayIssues(issues) {
     if (issues.length === 0) {
@@ -42,6 +55,10 @@ function getRepoIssues(repo) {
         if (response.ok) {
             response.json().then(function (data) {
                 displayIssues(data);
+
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
             alert("There was a problem with your request")
