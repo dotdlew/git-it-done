2e4d98da09bc304c70d58edfa4d345a6f0a0ccb5
Daniel Lewis <daniel.lewis@protonmail.com>
2021-02-25 14:32:00 -0700

    added repository querying and error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,7 +1,24 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
-function displayWarning(repo){
+function getRepoName() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+}
+
+function displayWarning(repo) {
     limitWarningEl.textContent = "To see more than 30 issues, visit ";
 
     var linkEl = document.createElement("a");
@@ -51,19 +68,23 @@ function displayIssues(issues) {
 function getRepoIssues(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
+    // make a get request to url
     fetch(apiUrl).then(function (response) {
+        // request was successful
         if (response.ok) {
             response.json().then(function (data) {
                 displayIssues(data);
 
+                // check if api has paginated issues
                 if (response.headers.get("Link")) {
                     displayWarning(repo);
                 }
             });
         } else {
-            alert("There was a problem with your request")
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
